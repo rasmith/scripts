6f982eaa929d4ea89ac07470b85fd836cd2c1fbb
Randall Smith <ransmith@amd.com>
2025-10-30 19:25:33 -0500

    remap detach keys
    Signed-off-by: Randall Smith <ransmith@amd.com>

--- devr.sh
+++ devr.sh
@@ -2,14 +2,30 @@
 
 # NOTE: Script assumes there is a $HOME/git directory to map to.
 
-USER_NAME=$(whoami)
+USER=$(whoami)
 
-CONTAINER=ransmith_dev_container
+IMAGE=${USER}_dev
+CONTAINER=${IMAGE}_container
+
+BUILD_OR_CREATE=$1
+
+if [[ -n $BUILD_OR_CREATE ]]; then
+  if [[ $BUILD_OR_CREATE = "build" ]]; then
+    echo "Building container: $CONTAINER ..."
+    docker build -f Dockerfile.dev -t ransmith_dev \
+                    --build-arg UID=$(id -g) \
+                    --build-arg GID=$(id -u) \
+                    --build-arg USER=$USER  .
+    exit
+  fi
+else
+  echo "Attempting to run container $CONTAINER.."
+fi
 
 if docker ps -a | grep -q $CONTAINER; then
   read -p "Container ($CONTAINER) already exists, delete it? " YESNO
   if [[ $YESNO = 'y' ]]; then
-    echo "Deleting container: $CONTAINER."
+    echo "Deleting container: $CONTAINER ..."
     docker rm -f $CONTAINER
   else
     echo "Container already exists and did not read 'y', so exiting."
@@ -18,6 +34,7 @@ if docker ps -a | grep -q $CONTAINER; then
 fi
 
 sudo docker run -it --ipc=host --device=/dev/kfd \
+    --detach-keys "ctrl-\,q" \
     --device=/dev/dri --shm-size=64G --cap-add=SYS_PTRACE \
     --security-opt seccomp=unconfined \
     --ulimit core=0:0 \
