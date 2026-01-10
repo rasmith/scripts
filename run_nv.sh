1d47ef78c463ff5480f8ac5073914cfb3fce714e
Randall Smith <Randall.Smith@amd.com>
2026-01-10 03:01:41 +0000

    update devr
    Signed-off-by: Randall Smith <Randall.Smith@amd.com>

--- run_nv.sh
+++ run_nv.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-docker  run -it --detach --name=ransmith_vllm_ci_ \
+docker  run -it --detach --name=ransmith_vllm_ci_latest \
           --gpus all  --ipc=host --entrypoint /bin/bash  \
           -v /data/:/data -v $HOME/git/vllm:/vllm -w /vllm \
-          public.ecr.aws/q9t5s3a7/vllm-ci-postmerge-repo:10ef65eded8187df92c370d6ffd7fd2b8a3c1d3c
-          #vllm/vllm-openai:latest
+          nvcr.io/nvidia/pytorch:25.12-py3
+          #nvcr.io/nvidia/pytorch:23.10-py3
+#          vllm/vllm-openai:latest
+          #public.ecr.aws/q9t5s3a7/vllm-ci-postmerge-repo:10ef65eded8187df92c370d6ffd7fd2b8a3c1d3c
