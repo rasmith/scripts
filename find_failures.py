eb524aaca582fd76be62bf19278a8d76ed40daa9
Randall Smith <Randall.Smith@amd.com>
2026-02-12 21:52:43 +0000

    update find faiulres script
    Signed-off-by: Randall Smith <Randall.Smith@amd.com>

--- find_failures.py
+++ find_failures.py
@@ -1,30 +1,30 @@
 import argparse
+import numpy as np
 
-
+SHORT_TEST_SUMMARY_INFO_LINE="=========================== short test summary info ============================"
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", type=str, default="", required=True)
     args = parser.parse_args()
     filename = args.file
+    failures = []
     with open(filename, "r") as f:
         lines = f.read().splitlines()
-        matches = filter(
-            lambda i: (lines[i].startswith("FAILED ") or lines[i] == "FAILED"),
-            range(len(lines)))
-        full_matches = map(
-            lambda i: (lines[i] + " " + lines[i + 1]
-                       if lines[i] == "FAILED" else lines[i]), matches)
-        failures = list(map(lambda m: m.split()[1], full_matches))
+        numpy_lines = np.array(lines)
+        summary_info_line_locations = np.where(numpy_lines == SHORT_TEST_SUMMARY_INFO_LINE)[0]
+        for location in summary_info_line_locations:
+            i = location + 1
+            while lines[i].startswith("FAILED"):
+                failures.append(lines[i])
+                i = i + 1
         print("-" * 10 + " FAILURES " + "-" * 10)
+        failures = sorted(failures)
         for failure in failures:
             print(failure)
-
-        failed_files = sorted(
-            list(set(map(lambda f: f.split("::")[1].split("[")[0], failures))))
-
-        print("=" * 50)
-        for failed_file in failed_files:
-            print(failed_file)
+    files = sorted(list(set((map(lambda x: x.split()[1].split("::")[0], failures)))))
+    print("-" * 10 + " FILES " + "-" * 10)
+    for f in files:
+        print(f)
 
 
 if __name__ == "__main__":
