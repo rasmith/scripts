89df1b6b857b2e4f521bd9d93490e64e428ac606
Randall Smith <Randall.Smith@amd.com>
2026-02-12 21:53:19 +0000

    format
    Signed-off-by: Randall Smith <Randall.Smith@amd.com>

--- find_failures.py
+++ find_failures.py
@@ -1,7 +1,9 @@
 import argparse
 import numpy as np
 
-SHORT_TEST_SUMMARY_INFO_LINE="=========================== short test summary info ============================"
+SHORT_TEST_SUMMARY_INFO_LINE = "=========================== short test summary info ============================"
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", type=str, default="", required=True)
@@ -11,7 +13,8 @@ def main():
     with open(filename, "r") as f:
         lines = f.read().splitlines()
         numpy_lines = np.array(lines)
-        summary_info_line_locations = np.where(numpy_lines == SHORT_TEST_SUMMARY_INFO_LINE)[0]
+        summary_info_line_locations = np.where(
+            numpy_lines == SHORT_TEST_SUMMARY_INFO_LINE)[0]
         for location in summary_info_line_locations:
             i = location + 1
             while lines[i].startswith("FAILED"):
@@ -21,7 +24,8 @@ def main():
         failures = sorted(failures)
         for failure in failures:
             print(failure)
-    files = sorted(list(set((map(lambda x: x.split()[1].split("::")[0], failures)))))
+    files = sorted(
+        list(set((map(lambda x: x.split()[1].split("::")[0], failures)))))
     print("-" * 10 + " FILES " + "-" * 10)
     for f in files:
         print(f)
